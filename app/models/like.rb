6d0e7ce68bb1973bac84acab3bba1057b5118822
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-13 12:22:37 -0500

    set after_create and after_destroy on Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,9 @@ class Like < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   belongs_to :post, foreign_key: 'post_id'
 
+  after_create :increment_like_counter
+  after_destroy :decrement_like_counter
+
   def increment_like_counter
     post.increment!(:likes_counter)
   end
