5d7b47b15dc2351f9a2eb2a46fea6715001d3dcd
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-13 17:09:25 -0500

    create post spec and update files extension

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  validation :title, presence: true, length: { maximum: 250 }
-  validation :comments_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
-  validation :likes_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 
   belongs_to :author, class_name: 'User'
   has_many :likes
