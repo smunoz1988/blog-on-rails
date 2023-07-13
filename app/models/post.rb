f92de5ee52ddd254e485060c374b859876aff0d6
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-13 11:50:03 -0500

    set recent_comments method on post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
