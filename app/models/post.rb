f9e2c8e9fcb39c5e142b1486eda1a70e03d00685
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-13 16:09:31 -0500

    validation for comments_counters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   validation :title, presence: true, length: { maximum: 250 }
-
+  validation :comments_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
 
   belongs_to :author, class_name: 'User'
   has_many :likes
