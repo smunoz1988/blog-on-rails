76b431ced8133809ca5ad13f290b8634a02f7622
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-14 09:48:17 -0500

    set test case for post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Post.new(title: 'This is a post', comments_counter: 2, likes_counter: 1) }
+
+  before { subject.save }
+
+  it 'Title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
 end
