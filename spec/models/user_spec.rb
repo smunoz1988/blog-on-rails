515c6c3d433749fe4d20780a74d707b374fb2861
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-13 19:55:41 -0500

    update user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'John Doe', posts_counter: 0) }
+  subject { User.create(name: 'Mike', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0) }
 
   before { subject.save }
 
@@ -13,5 +13,23 @@ RSpec.describe User, type: :model do
   it 'post_counter should be greater than or equal to 0' do
     subject.posts_counter = -3
     expect(subject).to_not be_valid
+  end
+
+  it 'post_counter should be greater than or equal to 0' do
+    subject.posts_counter = 3
+    expect(subject).to be_valid
+  end
+
+  describe '#recent_posts' do
+    it 'returns the most recent 3 posts' do
+      user = User.create(name: 'Mike', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)
+      post1 = Post.create(author: user, title: 'Hello', text: 'This is my first post')
+      post2 = Post.create(author: user, title: 'Hello2', text: 'This is my second post')
+      post3 = Post.create(author: user, title: 'Hello3', text: 'This is my third post')
+      post4 = Post.create(author: user, title: 'Hello4', text: 'This is my fourth post')
+      post5 = Post.create(author: user, title: 'Hello5', text: 'This is my fifth post')
+
+      user.recent_posts
+    end
   end
 end
