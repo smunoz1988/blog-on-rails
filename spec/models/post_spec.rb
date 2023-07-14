9aff413e5b187d89da035f3e3a65cf3e7a95b4cd
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-14 09:49:57 -0500

    increment and decrement methods test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -25,6 +25,24 @@ RSpec.describe Post, type: :model do
     expect(subject).to_not be_valid
   end
 
+  describe '#increment_post_counter' do
+    it 'increments the post_counter of the associated user' do
+      user = User.create(name: 'John Doe', posts_counter: 0)
+      post = Post.create(author_id: user.id, title: 'Second Post', text: 'More thoughts', comments_counter: 0,
+                         likes_counter: 0)
 
+      expect { post.increment_post_counter }.to change { user.reload.posts_counter }.by(1)
+    end
+  end
+
+  describe '#decrement_post_counter' do
+    it 'decrements the post_counter of the associated user' do
+      user = User.create(name: 'John Doe', posts_counter: 4)
+      post = Post.create(author_id: user.id, title: 'Second Post', text: 'More thoughts', comments_counter: 0,
+                         likes_counter: 0)
+
+      expect { post.decrement_post_counter }.to change { user.reload.posts_counter }.by(-1)
+    end
+  end
 
 end
