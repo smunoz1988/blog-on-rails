50f4a3474ea3353f8342f3f3b0ab025d8584a886
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-14 10:04:03 -0500

    set cooment test case and ad test for incre com

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe '#increment_comments_counter' do
+    it 'increments the likes_counter of the associated post' do
+      user = User.create(name: 'John Doe', posts_counter: 0)
+      post = Post.create(author_id: user.id, title: 'Second Post', text: 'comments', comments_counter: 0,
+                         likes_counter: 0)
+
+      comment = Comment.create(author_id: user.id, post_id: post.id, text: 'im happy im sharing my comments!')
+      expect { comment.increment_comment_counter }.to change { post.reload.comments_counter }.by(1)
+    end
+  end
+end
