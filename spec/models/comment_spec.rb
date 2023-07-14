c1bc3d91b1c544459ba3999cc20fb0add077dc37
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-14 10:04:17 -0500

    ad test for decrement comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe Comment, type: :model do
       expect { comment.increment_comment_counter }.to change { post.reload.comments_counter }.by(1)
     end
   end
+
+  describe '#decrement_comments_counter' do
+    it 'decrements the likes_counter of the associated post' do
+      user = User.create(name: 'John Doe', posts_counter: 0)
+      post = Post.create(author_id: user.id, title: 'Second Post', text: 'More thoughts', comments_counter: 2,
+                         likes_counter: 2)
+
+      comment = Comment.create(author_id: user.id, post_id: post.id, text: 'im happy im sharing my comments!')
+      expect { comment.decrement_comment_counter }.to change { post.reload.comments_counter }.by(-1)
+    end
+  end
 end
