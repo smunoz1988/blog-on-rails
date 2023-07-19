7e6913cf41386233f66faf9f0d3ba540e51fb435
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-19 12:45:41 -0500

    create index view for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get '/users', to: 'users#index, as: 'users'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
