0414812823afdb55ac7565e4dc3c7b56fde3d19b
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-19 12:38:26 -0500

    set route to users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/users', to: 'users#index, as: 'users'
 end
