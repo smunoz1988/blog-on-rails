f729314108e4eb8743caf2e4f1d9994113324259
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-19 17:15:25 -0500

    test for status correct in posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  describe 'GET /posts' do
+    it 'works!' do
+      get '/users/1/posts'
+      expect(response).to have_http_status(200)
+    end
+  end
 end
