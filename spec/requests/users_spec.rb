325841e5f45a452afc28b53d6a05a877f50d4fd7
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-19 17:03:31 -0500

    test for template works on users/id

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -22,4 +22,8 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+  it 'template works!' do
+    get user_path(1)
+    expect(response).to render_template(:show)
+  end
 end
