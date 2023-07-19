2c0a62047870890e993e7ebeb6864b79a0ed6a00
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-19 16:52:13 -0500

    test for check if correct template was rentered

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe 'Users', type: :request do
       get users_path
       expect(response).to have_http_status(200)
     end
+    it 'template works!' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
   end
 end
