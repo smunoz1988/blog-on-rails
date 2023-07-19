0955841f939d7ba29ea3114bb7fa4dc00c11af38
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-19 17:21:28 -0500

    test for render template on posts/id

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -21,5 +21,9 @@ RSpec.describe 'Posts', type: :request do
       get '/users/1/posts/1'
       expect(response).to have_http_status(200)
     end
+    it 'template works!' do
+      get '/users/1/posts/1'
+      expect(response).to render_template(:show)
+    end
   end
 end
