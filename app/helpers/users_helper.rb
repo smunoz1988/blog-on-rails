ece52d9070d34a3ca868bfd92faec499b4ce5e3a
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-20 13:32:57 -0500

    set Users helper to get users data

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def user_details(user)
+    content_tag(:div, class: 'user_details') do
+      concat (content_tag(:h2, link_to(user.name, user_path(user), class: 'user_name')))
+      concat (content_tag(:p, "Number of posts: #{user.posts_counter}", class: 'user_post_counter'))
+    end
+  end
 end
