210588cbbc14a49ee87f5fc43ed4d3e2cdfe9598
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-20 17:53:35 -0500

    update user index style

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
   def user_details(user)
-    content_tag(:div, class: 'user_details') do
-      concat(content_tag(:h2, link_to(user.name, user_path(user), class: 'user_name')))
-      concat(content_tag(:p, "Number of posts: #{user.posts_counter}", class: 'user_post_counter'))
+    content_tag(:div, class: 'userDetails') do
+      concat(content_tag(:h2, link_to(user.name, user_path(user), class: 'userName')))
+      concat(content_tag(:p, "Number of posts: #{user.posts_counter}", class: 'postsCounter'))
     end
   end
 end
