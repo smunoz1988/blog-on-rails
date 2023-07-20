ae51e6a2be4424e4cc4942c419e451c70ff2698e
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-20 16:05:35 -0500

    add post helper to deploy comments data by user

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,15 @@
 module PostsHelper
+    def render_comments_section(post)
+        content_tag(:ul, class: 'ul-comments-box') do
+          concat content_tag(:h4, 'Comments:', class: 'section-comments')
+    
+          if post.comments.blank?
+            concat content_tag(:li, 'no comments for the moment')
+          else
+            post.recent_comments.each do |comment|
+              concat content_tag(:li, "Username: #{comment.text.blank? ? 'no comments for the moment' : comment.text}")
+            end
+          end
+        end
+      end
 end
