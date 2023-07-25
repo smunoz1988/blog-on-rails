17c62fdf529f30d532abcae3095925b56f02ef07
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-25 15:39:04 -0500

    linter fix

--- spec/system/post_show_spec.rb
+++ spec/system/post_show_spec.rb
@@ -10,18 +10,22 @@ RSpec.describe 'Post Show', type: :system do
   post = Post.create(title: 'My First Post', text: 'This is my first post', author_id: user.id, comments_counter: 0,
                      likes_counter: 0)
   comment = Comment.create(text: 'This is my first comment', author_id: user.id, post_id: post.id)
+
   it 'displays posts title' do
     visit user_post_path(user_id: user.id, id: post.id)
     expect(page).to have_content(post.title)
   end
+
   it 'displays who wrote the post' do
     visit user_post_path(user_id: user.id, id: post.id)
     expect(page).to have_content(user.name)
   end
+
   it 'displays how many comments the post has' do
     visit user_post_path(user_id: user.id, id: post.id)
     expect(page).to have_content(post.comments_counter)
   end
+
   it 'displays how many likes the post has' do
     visit user_post_path(user_id: user.id, id: post.id)
     expect(page).to have_content(post.likes_counter)
