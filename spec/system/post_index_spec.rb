374bd274fc6d3551a8d29ece1526a80bd25e49e6
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-25 15:15:40 -0500

    post index test display number of likes

--- spec/system/post_index_spec.rb
+++ spec/system/post_index_spec.rb
@@ -58,4 +58,13 @@ RSpec.describe 'Post Index', type: :system do
 
     expect(page).to have_content(post.comments_counter)
   end
+
+  it 'displays the number of likes on a post' do
+    post = Post.create(title: 'My First Post', text: 'This is my first post', author_id: user.id,
+                       comments_counter: 0, likes_counter: 0)
+    Like.create(author_id: user.id, post_id: post.id)
+    visit user_posts_path(user_id: user.id)
+
+    expect(page).to have_content(post.likes_counter)
+  end
 end
