8d5ab29ac7ad27839c28de92b96c0c7ceb5b5a78
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-25 15:11:36 -0500

    post index test, show picture

--- spec/system/post_index_spec.rb
+++ spec/system/post_index_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe 'Post Index', type: :system do
   end
 
   user = User.create(name: 'John Doe', photo: 'https://short.url/example', posts_counter: 5)
+
+  it "displays the user's profile picture on user posts page" do
+    visit user_posts_path(user_id: user.id)
+
+    expect(page).to have_css("img[src='#{user.photo}']")
+  end
 end
