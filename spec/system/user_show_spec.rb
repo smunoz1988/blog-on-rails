be2e6ed29b45e05e881a8a528326fc654babd3be
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-25 11:20:58 -0500

    update userShow test to match bio

--- spec/system/user_show_spec.rb
+++ spec/system/user_show_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe 'User Show Page', type: :system do
     User.create(
       name: 'Mike Patton',
       photo: 'https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80',
+      bio: 'I am a musician',
       posts_counter: 3
     )
   end
