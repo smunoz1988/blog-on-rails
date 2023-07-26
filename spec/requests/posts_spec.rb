716beda22a64e03b85c7fbc56bcbe99dd0182ba6
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-25 21:59:20 -0500

    post test update path

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   let(:user) do
     User.create(
-      id: 1,
       name: 'Mike Patton',
       photo: 'https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80',
       bio: 'I am a musician',
@@ -13,9 +12,8 @@ RSpec.describe 'Posts', type: :request do
 
   let(:post) do
     Post.create(
-      id: 1,
       title: 'My First Post',
-      text: 'This is my first post',
+      text: 'This is my first test',
       author: user,
       comments_counter: 0,
       likes_counter: 0
@@ -29,31 +27,31 @@ RSpec.describe 'Posts', type: :request do
 
   describe 'GET /posts' do
     it 'works!' do
-      get '/users/1/posts'
+      get user_posts_path(user_id: user.id)
       expect(response).to have_http_status(200)
     end
     it 'template works!' do
-      get '/users/1/posts'
+      get user_posts_path(user_id: user.id)
       expect(response).to render_template(:index)
     end
     it 'the response body includes correct placeholder text' do
-      get '/users/1/posts'
+      get user_posts_path(user_id: user.id)
       expect(response.body).to include('Mike Patton')
     end
   end
 
   describe 'GET /posts/:id' do
     it 'works!' do
-      get '/users/1/posts/1'
+      get user_post_path(user_id: user.id, id: post.id)
       expect(response).to have_http_status(200)
     end
     it 'template works!' do
-      get '/users/1/posts/1'
+      get user_post_path(user_id: user.id, id: post.id)
       expect(response).to render_template(:show)
     end
     it 'the response body includes correct placeholder text' do
-      get '/users/1/posts/1'
-      expect(response.body).to include('This is my first post')
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response.body).to include('This is my first test')
     end
   end
 end
