afb2575034df05ff4dd06d58dc71489bf14071f8
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-25 19:52:35 -0500

    create user on posts_spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,24 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  let(:user) do
+    User.create(
+      id: 1,
+      name: 'Mike Patton',
+      photo: 'https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80',
+      bio: 'I am a musician',
+      posts_counter: 3
+    )
+  end
+
+  before do
+    user
+  end
+
   describe 'GET /posts' do
     it 'works!' do
-      get '/users/514/posts'
+      get '/users/1/posts'
       expect(response).to have_http_status(200)
     end
     it 'template works!' do
-      get '/users/514/posts'
+      get '/users/1/posts'
       expect(response).to render_template(:index)
     end
     it 'the response body includes correct placeholder text' do
-      get '/users/514/posts'
-      expect(response.body).to include('John Doe')
+      get '/users/1/posts'
+      expect(response.body).to include('Mike Patton')
     end
   end
 
   describe 'GET /posts/:id' do
     it 'works!' do
-      get '/users/514/posts/404'
+      get '/users/1/posts/404'
       expect(response).to have_http_status(200)
     end
     it 'template works!' do
