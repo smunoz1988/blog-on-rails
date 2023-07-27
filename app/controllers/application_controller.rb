dbb1741a9479e69d311a62c5020fd056c7bfdb96
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-27 13:58:30 -0500

    add field to user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio])
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :name, :bio)
     end
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password) }
+  end
 end
