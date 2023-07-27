65242ed362f75555ee0f6f7e6c40d9c7d303e25f
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-27 13:58:53 -0500

    set user to confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
 
   attribute :posts_counter, :integer, default: 0
+  attribute :photo, :string, default: 'https://cdn-icons-png.flaticon.com/512/6075/6075889.png'
 
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
