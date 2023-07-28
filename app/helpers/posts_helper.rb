69023be9756ce49eaac621d861c0cdcfe68c9919
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-28 14:58:56 -0500

    update post helper to manage likes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -8,10 +8,7 @@ module PostsHelper
       else
         post.recent_comments.each do |comment|
           concat content_tag(:li, "#{comment.author.name}: #{comment.text.blank? ? 'no comments for the moment' : comment.text}") do
-            # Add delete button here, conditionally if the user can delete the comment
-            if can?(:destroy, comment)
-              concat button_to("Delete", user_post_comment_path(user_id: comment.author.id, post_id: comment.post.id, id: comment.id), method: :delete, class: "btn")
-            end
+          concat button_to("Delete", user_post_comment_path(user_id: comment.author.id, post_id: comment.post.id, id: comment.id), method: :delete, class: "btn")
           end
         end
       end
@@ -22,7 +19,7 @@ module PostsHelper
     content_tag(:div, class: 'text') do
       concat content_tag(:p, "Comments: #{post.comments_counter.nil? ? 0 : post.comments_counter}",
                          class: 'comment-text')
-      concat link_to 'Likes: ', increment_likes_path(post), method: :post, remote: true, class: 'like-counter',
+      concat link_to 'Likes: ', user_post_likes_path(user_id: @post.author_id, post_id: @post.id), method: :post, remote: true, class: 'like-counter',
                                                             id: "like-link-#{post.id}"
       concat content_tag(:span, post.likes_counter.nil? ? 0 : post.likes_counter, class: 'like-counter-number',
                                                                                   id: "like-counter-#{post.id}")
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :users
   root 'users#index'
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create destroy] do
       resources :comments, only: %i[new create destroy]
+      resources :likes, only: %i[new create]
+      end
     end
-  end
-
-  get '/posts/:id/increment_likes', to: 'posts#increment_likes', as: 'increment_likes'
 end
