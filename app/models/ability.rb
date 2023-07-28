a3c2b5afd35b44b13b82bca6986c050e4bbb7fae
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-07-28 11:09:42 -0500

    manage authorizations on ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,22 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, Post, author_id: user.id
+      can :create, Comment, author_id: user.id
+      can :create, Like
+      can :destroy, Post do |post|
+        post.author_id == user.id
+      end
+      can :destroy, Comment do |comment|
+        comment.author_id == user.id
+      end
+    end    
+  end
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
